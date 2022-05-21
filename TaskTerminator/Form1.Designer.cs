300800e7677e2e4031b98242ed386315a20b331d
Tutislav <tutislav@outlook.cz>
2022-05-21 23:12:51 +0200

    Added process view, stop and terminate

--- TaskTerminator/Form1.Designer.cs
+++ TaskTerminator/Form1.Designer.cs
@@ -32,14 +32,20 @@ namespace TaskTerminator
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip = new System.Windows.Forms.MenuStrip();
-            this.statusStrip = new System.Windows.Forms.StatusStrip();
-            this.listView = new System.Windows.Forms.ListView();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.listView = new System.Windows.Forms.ListView();
             this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.terminateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nameColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cpuColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ramColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pidColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timer = new System.Windows.Forms.Timer(this.components);
             this.menuStrip.SuspendLayout();
             this.contextMenuStrip.SuspendLayout();
             this.SuspendLayout();
@@ -54,25 +60,6 @@ namespace TaskTerminator
             this.menuStrip.TabIndex = 0;
             this.menuStrip.Text = "menuStrip1";
             // 
-            // statusStrip
-            // 
-            this.statusStrip.Location = new System.Drawing.Point(0, 739);
-            this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Size = new System.Drawing.Size(784, 22);
-            this.statusStrip.TabIndex = 1;
-            this.statusStrip.Text = "statusStrip1";
-            // 
-            // listView
-            // 
-            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView.HideSelection = false;
-            this.listView.Location = new System.Drawing.Point(0, 24);
-            this.listView.Name = "listView";
-            this.listView.Size = new System.Drawing.Size(784, 715);
-            this.listView.TabIndex = 2;
-            this.listView.UseCompatibleStateImageBehavior = false;
-            this.listView.View = System.Windows.Forms.View.Details;
-            // 
             // fileToolStripMenuItem
             // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -85,14 +72,40 @@ namespace TaskTerminator
             // runToolStripMenuItem
             // 
             this.runToolStripMenuItem.Name = "runToolStripMenuItem";
-            this.runToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
             this.runToolStripMenuItem.Text = "Run";
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
             this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Location = new System.Drawing.Point(0, 739);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nameColumnHeader,
+            this.statusColumnHeader,
+            this.cpuColumnHeader,
+            this.ramColumnHeader,
+            this.pidColumnHeader});
+            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(0, 24);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(784, 715);
+            this.listView.TabIndex = 2;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView_MouseClick);
             // 
             // contextMenuStrip
             // 
@@ -100,19 +113,52 @@ namespace TaskTerminator
             this.stopToolStripMenuItem,
             this.terminateToolStripMenuItem});
             this.contextMenuStrip.Name = "contextMenuStrip";
-            this.contextMenuStrip.Size = new System.Drawing.Size(127, 48);
+            this.contextMenuStrip.Size = new System.Drawing.Size(181, 70);
             // 
             // stopToolStripMenuItem
             // 
             this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
-            this.stopToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
             // 
             // terminateToolStripMenuItem
             // 
             this.terminateToolStripMenuItem.Name = "terminateToolStripMenuItem";
-            this.terminateToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.terminateToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.terminateToolStripMenuItem.Text = "Terminate";
+            this.terminateToolStripMenuItem.Click += new System.EventHandler(this.terminateToolStripMenuItem_Click);
+            // 
+            // nameColumnHeader
+            // 
+            this.nameColumnHeader.Text = "Name";
+            this.nameColumnHeader.Width = 400;
+            // 
+            // statusColumnHeader
+            // 
+            this.statusColumnHeader.Text = "Status";
+            this.statusColumnHeader.Width = 80;
+            // 
+            // cpuColumnHeader
+            // 
+            this.cpuColumnHeader.Text = "CPU";
+            this.cpuColumnHeader.Width = 80;
+            // 
+            // ramColumnHeader
+            // 
+            this.ramColumnHeader.Text = "RAM";
+            this.ramColumnHeader.Width = 80;
+            // 
+            // pidColumnHeader
+            // 
+            this.pidColumnHeader.Text = "PID";
+            this.pidColumnHeader.Width = 50;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 2000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // Form1
             // 
@@ -127,6 +173,7 @@ namespace TaskTerminator
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TaskTerminator";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
             this.contextMenuStrip.ResumeLayout(false);
@@ -146,6 +193,12 @@ namespace TaskTerminator
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
         private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem terminateToolStripMenuItem;
+        private System.Windows.Forms.ColumnHeader nameColumnHeader;
+        private System.Windows.Forms.ColumnHeader statusColumnHeader;
+        private System.Windows.Forms.ColumnHeader cpuColumnHeader;
+        private System.Windows.Forms.ColumnHeader ramColumnHeader;
+        private System.Windows.Forms.ColumnHeader pidColumnHeader;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- TaskTerminator/Form1.cs
+++ TaskTerminator/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,111 @@ namespace TaskTerminator
         public Form1()
         {
             InitializeComponent();
+        }
+        List<Process> processesList;
+        List<Process> currentProcessesList;
+
+        private void process_Add(Process process)
+        {
+            String processName = process.ProcessName;
+            if (process.MainWindowTitle != "") processName = process.MainWindowTitle;
+            String processStatus = "running";
+            if (!process.Responding) processStatus = "not responding";
+            ListViewItem processInfo = new ListViewItem(processName);
+            processInfo.SubItems.Add(processStatus);
+            processInfo.SubItems.Add("");
+            processInfo.SubItems.Add("");
+            processInfo.SubItems.Add(process.Id.ToString());
+            if (process.MainWindowTitle != "") listView.Items.Insert(0, processInfo);
+            else listView.Items.Add(processInfo);
+        }
+
+        private void process_Update(Process process)
+        {
+            String processName = process.ProcessName;
+            if (process.MainWindowTitle != "") processName = process.MainWindowTitle;
+            String processStatus = "running";
+            if (!process.Responding) processStatus = "not responding";
+            ListViewItem processInfo = listView.Items.Cast<ListViewItem>().Where(l => l.SubItems[l.SubItems.Count - 1].Text == process.Id.ToString()).First();
+            if (processInfo.Text != processName) processInfo.Text = processName;
+            if (processInfo.SubItems[1].Text != processStatus) processInfo.SubItems[1].Text = processStatus;
+        }
+
+        private void process_Remove(Process process)
+        {
+            listView.Items.Remove(listView.Items.Cast<ListViewItem>().Where(l => l.SubItems[l.SubItems.Count - 1].Text == process.Id.ToString()).First());
+        }
+
+        private void cmd(String command, bool asAdmin)
+        {
+            Process process = new Process();
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = "cmd.exe";
+            processStartInfo.Arguments = "/C " + command;
+            processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            if (asAdmin) processStartInfo.Verb = "runas";
+            process.StartInfo = processStartInfo;
+            process.Start();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            processesList = Process.GetProcesses().ToList();
+            foreach (Process process in processesList)
+            {
+                process_Add(process);
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            currentProcessesList = Process.GetProcesses().ToList();
+            foreach (Process process in currentProcessesList)
+            {
+                if (!processesList.Exists(p => p.Id == process.Id))
+                {
+                    process_Add(process);
+                }
+                else
+                {
+                    process_Update(process);
+                }
+            }
+            foreach (Process process in processesList)
+            {
+                if (!currentProcessesList.Exists(p => p.Id == process.Id))
+                {
+                    process_Remove(process);
+                }
+            }
+            processesList = new List<Process>(currentProcessesList);
+        }
+
+        private void listView_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (listView.FocusedItem.Bounds.Contains(e.Location))
+                {
+                    contextMenuStrip.Show(Cursor.Position);
+                }
+            }
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ListViewItem processInfo = listView.FocusedItem;
+            int processId = int.Parse(processInfo.SubItems[processInfo.SubItems.Count - 1].Text);
+            Process process = processesList.Find(p => p.Id == processId);
+            process.Kill();
+        }
+
+        private void terminateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ListViewItem processInfo = listView.FocusedItem;
+            int processId = int.Parse(processInfo.SubItems[processInfo.SubItems.Count - 1].Text);
+            Process process = processesList.Find(p => p.Id == processId);
+            cmd("taskkill /PID " + processId + " /T /F", true);
         }
     }
 }
